d54cceef186921f26c76e3845f8f76aedd9d5dc3
Harry Chang <harry830622@gmail.com>
2020-08-15 21:32:45 +0800

    Display gas price in USD

--- src/popup/components/App/App.jsx
+++ src/popup/components/App/App.jsx
@@ -2,27 +2,84 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import { jsx, css } from '@emotion/core';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import {
+  Container,
+  Grid,
   Paper,
   AppBar,
   Toolbar,
   IconButton,
   Typography,
 } from '@material-ui/core';
-import { Refresh as RefreshIcon } from '@material-ui/icons';
+import { Skeleton } from '@material-ui/lab';
+import {
+  Settings as SettingsIcon,
+  Refresh as RefreshIcon,
+} from '@material-ui/icons';
 
 const { ORIGIN } = process.env;
+
+const gasConfigs = [
+  {
+    key: 'fast',
+    title: 'Fast',
+  },
+  {
+    key: 'average',
+    title: 'Average',
+  },
+  {
+    key: 'safeLow',
+    title: 'Slow',
+  },
+];
+
+const gasLimitByTxType = {
+  transfer: 21000,
+};
+
+const symbolByCurrency = {
+  eth: 'Ξ',
+  btc: '₿',
+  twd: 'NT$',
+  usd: '$',
+  cad: '$',
+  aud: '$',
+  cny: '¥',
+  jpy: '¥',
+  sgd: '$',
+  thb: '฿',
+  vnd: '₫',
+  eur: '€',
+  gbp: '£',
+  inr: '₹',
+  krw: '₩',
+  hkd: 'HK$',
+};
 
 const App = () => {
   const [isFetching, setIsFetching] = useState(true);
   const [gas, setGas] = useState({});
+  const [ethPrice, setEthPrice] = useState(0);
   useEffect(() => {
     if (isFetching) {
-      axios.get(`${ORIGIN}/api/gas/now`).then((res) => {
-        setGas(res.data);
-        setIsFetching(false);
-      });
+      axios
+        .get(`${ORIGIN}/api/gas/now`)
+        .then((res) => {
+          setGas(res.data);
+        })
+        .then(() =>
+          axios
+            .get(
+              `https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd`,
+            )
+            .then((res) => {
+              setEthPrice(res.data.ethereum.usd);
+            }),
+        )
+        .then(() => {
+          setIsFetching(false);
+        });
     }
   }, [isFetching]);
 
@@ -31,69 +88,105 @@ const App = () => {
   }, []);
 
   return (
-    <>
-      <CssBaseline />
-      <div
-        css={css`
-          width: 300px;
-        `}
-      >
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6">⛽</Typography>
-            <div
-              css={css`
-                flex-grow: 1;
-              `}
-            />
-            <IconButton color="inherit" onClick={handleRefreshClick}>
-              <RefreshIcon />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <div
+    <div
+      css={css`
+        width: 400px;
+      `}
+    >
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6">⛽</Typography>
+          <div
+            css={css`
+              flex-grow: 1;
+            `}
+          />
+          <IconButton color="inherit">
+            <SettingsIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <main
           css={css`
-            display: flex;
-            justify-content: space-evenly;
-            padding: 10px 0px;
+            padding: 68px 0 20px;
           `}
         >
-          {isFetching ? null : (
-            <>
-              {[
-                {
-                  key: 'fast',
-                  title: 'FAST',
-                },
-                {
-                  key: 'average',
-                  title: 'AVG',
-                },
-                {
-                  key: 'safeLow',
-                  title: 'SAFE',
-                },
-              ].map(({ key, title }) => (
-                <Paper
-                  key={key}
+          <Paper
+            variant="outlined"
+            css={css`
+              padding: 16px;
+            `}
+          >
+            {isFetching ? (
+              <Skeleton
+                variant="rect"
+                animation="wave"
+                css={css`
+                  height: 100px;
+                `}
+              />
+            ) : (
+              <>
+                <div
                   css={css`
-                    flex-basis: 30%;
                     display: flex;
-                    flex-direction: column;
-                    justify-content: center;
                     align-items: center;
-                    padding: 10px 0;
                   `}
                 >
-                  <div>{gas[key]}</div>
-                  <Typography variant="subtitle1">{title}</Typography>
-                </Paper>
-              ))}
-            </>
-          )}
-        </div>
-      </div>
-    </>
+                  <div
+                    css={css`
+                      flex-grow: 0;
+                    `}
+                  >
+                    <Typography variant="subtitle1">
+                      A transfer will cost you
+                    </Typography>
+                  </div>
+                  <div
+                    css={css`
+                      flex-grow: 1;
+                      display: flex;
+                      flex-direction: row-reverse;
+                    `}
+                  >
+                    <IconButton color="inherit" onClick={handleRefreshClick}>
+                      <RefreshIcon />
+                    </IconButton>
+                  </div>
+                </div>
+                <Grid container spacing={2}>
+                  {gasConfigs.map(({ key, title }) => (
+                    <Grid key={key} item xs={4}>
+                      <Paper
+                        variant="outlined"
+                        css={css`
+                          display: flex;
+                          flex-direction: column;
+                          justify-content: center;
+                          align-items: center;
+                          padding: 10px 5px;
+                        `}
+                      >
+                        <Typography variant="subtitle2">
+                          {`${symbolByCurrency.usd}${(
+                            gasLimitByTxType.transfer *
+                            gas[key] *
+                            1e-9 *
+                            ethPrice
+                          ).toFixed(2)}`}
+                        </Typography>
+                        <Typography variant="body2">{title}</Typography>
+                      </Paper>
+                    </Grid>
+                  ))}
+                </Grid>
+              </>
+            )}
+          </Paper>
+        </main>
+      </Container>
+    </div>
   );
 };
 
--- src/popup/index.jsx
+++ src/popup/index.jsx
@@ -2,7 +2,14 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#react'));
+ReactDOM.render(
+  <>
+    <CssBaseline />
+    <App />
+  </>,
+  document.querySelector('#react'),
+);
