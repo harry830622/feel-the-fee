4e1e6765c59e43f6a9b5e2ff0e4d3a6ede9d2d72
Harry Chang <harry830622@gmail.com>
2020-08-30 15:40:29 +0800

    Add CurveDAO

--- src/constants/index.js
+++ src/constants/index.js
@@ -17,9 +17,13 @@ export const symbolByCurrency = {
   hkd: 'HK$',
 };
 
+// TODO: Move it to backend so that we don't need to update frontend code just
+// for adding a new txType
 export const methodsByContractName = {
   yearn__vault__yCrv: ['deposit', 'withdraw'],
   curve__pool__y: ['add_liquidity', 'remove_liquidity'],
+  curveDao__gauge__yCrv: ['deposit', 'withdraw'],
+  curveDao__minter: ['mint'],
 };
 
 export const txTypeByMethodByContractName = Object.entries(
@@ -64,6 +68,12 @@ export const textByTxType = {
     'Deposit to Curve Y pool',
   [txTypeByMethodByContractName.curve__pool__y.remove_liquidity]:
     'Withdraw from Curve Y pool',
+  [txTypeByMethodByContractName.curveDao__gauge__yCrv.deposit]:
+    'Deposit to CurveDAO yCRV gauge',
+  [txTypeByMethodByContractName.curveDao__gauge__yCrv.withdraw]:
+    'Withdraw to CurveDAO yCRV gauge',
+  [txTypeByMethodByContractName.curveDao__minter.mint]:
+    'Claim CRV from CurveDAO Minter',
 };
 
 export const textBySpeed = {
